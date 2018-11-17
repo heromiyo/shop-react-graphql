b0de9885c7a319f9efa7456af3fa86479c58fdba
francis miyoba <francismiyoba@gmail.com>
2018-11-17 12:38:51 +0800

    schema

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,15 @@
-// let's go!
+// this is the entry point for the node server
+require('dotenv').config({
+    path: 'variables.env'
+})
+const createServer = require('./createServer')
+const db = require('./db')
+
+const server = createServer()
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    },
+}, boot => console.log(`Server running on http://localhost:${boot.port}`))
