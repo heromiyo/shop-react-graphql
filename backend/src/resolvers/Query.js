10fb1ac326a6747a37cc10e4c780e599c012b62d
francis miyoba <francismiyoba@gmail.com>
2018-11-17 14:00:34 +0800

    querying items without need for special resolver logic simplification using forwardTo

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -9,11 +9,17 @@
  */
 
 // for how it connects to the prisma database and query data see the comment in src/resolvers/Mutation.js
+const { forwardTo } = require('prisma-binding')
 const Query = {
-    async items(parent, args, ctx, info) {
-        const item = await ctx.db.query.items()
-        return item
-    }
+    // async items(parent, args, ctx, info) {
+    //     const item = await ctx.db.query.items()
+    //     return item
+    // }
+    /*
+    if fetching items from db does not require special logic ie authentication etc
+    we can just grab the data directly from prisma by using forwardTo
+     */
+    items: forwardTo('db')
 }
 
 module.exports = Query
