1f5934bb734b637d3176d6f851bf07c734eca34f
francis miyoba <francismiyoba@gmail.com>
2018-11-17 13:50:53 +0800

    specify query and mutation in schema.graphql , use import comment to grab the types from prisma.graphql and then write resolver logic for mutations and queries in src/resolvers

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,29 @@
-const Mutations = {};
+// this Mutation file is connected to src/schema.graphql in the createServer file
+/*
+ return new GraphQLServer({
+        typeDefs: 'src/schema.graphql',
+        resolvers: {
+            Mutation,
+            Query
+        },
+ */
+const Mutations = {
+    async createItem(parent, args, ctx, info) {
+        /*
+        here we start to interface with the prisma API
+        for mutations we target the Mutation field in prisma.graphql
+        excerpt of the file -->
+        type Mutation {
+                   createUser(data: UserCreateInput!): User!
+                   createItem(data: ItemCreateInput!): Item!
+                   updateUser(data: UserUpdateInput!, where: UserWhereUniqueInput!): User
+                   updateItem(data: ItemUpdateInput!, where: ItemWhereUniqueInput!): Item
+         */
+        const item = await  ctx.db.mutation.createItem({
+            data: { ...args }
+        }, info  /* makes sure that the actual item we created is returned */)
+        return item
+    }
+}
 
-module.exports = Mutations;
+module.exports = Mutations
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,19 @@
-const Query = {};
+// this Query file is connected to src/schema.graphql in the createServer file
+/*
+ return new GraphQLServer({
+        typeDefs: 'src/schema.graphql',
+        resolvers: {
+            Mutation,
+            Query
+        },
+ */
 
-module.exports = Query;
+// for how it connects to the prisma database and query data see the comment in src/resolvers/Mutation.js
+const Query = {
+    async items(parent, args, ctx, info) {
+        const item = await ctx.db.query.items()
+        return item
+    }
+}
+
+module.exports = Query
