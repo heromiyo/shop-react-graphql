f520e0084f9ba50e09e262f0e5c2b8d7188999f6
francis miyoba <francismiyoba@gmail.com>
2018-11-17 14:42:10 +0800

    expose mutation and query data to the props on each page

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,13 +4,22 @@ import { ApolloProvider } from 'react-apollo'
 import withData from '../lib/withData'
 
 class Myapp extends App {
+    // to get page numbers like myapp.com/sell?page=2
+    static async getInitialProps({ Component, ctx}) {
+        let pageProps = {}
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx)
+        }
+        pageProps.query = ctx.query
+        return { pageProps } // expose it via props --> read next.js getInitialProps
+    }
     render() {
-        const { Component, apollo } = this.props
+        const { Component, apollo, pageProps  } = this.props
         return (
             <Container>
                 <ApolloProvider client={ apollo }>
                     <Page>
-                        <Component />
+                        <Component { ...pageProps }/>
                     </Page>
                 </ApolloProvider>
             </Container>
