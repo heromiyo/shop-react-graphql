a7d2b7f062331528ede1a6e453f9aaaba39be42e
francis miyoba <francismiyoba@gmail.com>
2018-11-27 14:49:06 +0800

    display single item

---
+++ frontend/components/SingleItem.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+import styled from 'styled-components'
+
+const SingleItemStyle = styled.div`
+    max-width: 1200px;
+    margin: 2rem auto;
+    box-shadow: ${props => props.theme.bs};
+    display: grid;
+    grid-auto-columns: 1fr;
+    grid-auto-flow: column;
+    min-height: 800px;
+`
+
+const SINGLE_ITEM_QUERY = gql`
+ query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: { id: $id }) {
+        id
+        title
+        description
+        largeImage 
+    }
+ }
+`
+class SingleItem extends Component {
+    render() {
+        return (
+            <Query query={ SINGLE_ITEM_QUERY } variables={{
+                id: this.props.id
+            }}>
+                {( { error, loading, data }) => (
+                    <SingleItemStyle>
+                        <img src={ data.item.largeImage } alt={ data.item.title }/>
+                    </SingleItemStyle>
+                ) }
+            </Query>
+        )
+    }
+}
+export default SingleItem
---
+++ frontend/pages/item.js
@@ -0,0 +1,5 @@
+import SingleItem from '../components/SingleItem'
+const Item = props => (
+    <SingleItem id={ props.query.id }></SingleItem>
+)
+export default Item
